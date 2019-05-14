a857694cd3d981c4e6e501e69a43b9c1d16a5819
Yuval Allweil <themarquisdesheric@gmail.com>
2019-05-13 20:45:16 -0700

    add logo, hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,50 @@
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
-import React from 'react';
 import PropTypes from 'prop-types';
+
+import Signature from '../images/signature.svg';
 
 const links = ['portfolio', 'about', 'contact'];
 
-// TODO: link underline transition on hover
+const NavLinks = ({ visible }) => 
+  <div className={`hamburger-list flex-col p-1 absolute ${visible ? 'flex' : 'hidden'} sm:flex sm:flex-row sm:static`}>
+    {links.map(link => 
+      <Link 
+        to={`/${link}`}
+        className="uppercase sm:inline sm:mr-4 sm:text-black"
+        key={link}
+      >
+        {link}
+      </Link>  
+    )}
+  </div>;
+
 // TODO: create pages programmatically
 // TODO: create galleries programmatically
 
-const Header = ({ siteTitle = '' }) => (
-  <header className="bg-black flex items-center justify-between h-12">
-    <h1 className="text-xl sm:text-2xl m-0 px-2 sm:px-4">
-      <Link to="/">
-        {siteTitle}
-      </Link>
-    </h1>
-    <nav className="text-xs">
-      {links.map(link => 
-        <Link 
-          to={`/${link}`}
-          className="pr-2 sm:pr-4 uppercase"
-          key={link}
-        >
-          {link}
-        </Link>  
-      )}
-    </nav>
-  </header>
-);
+const HamburgerMenu = ({ className, handleSetVisible }) => 
+  <div onClick={() => handleSetVisible(visible => !visible)} className={className}>
+    {/* replace with asset when available */}
+    {[1,2,3].map(num => <div className="hamburger w-8 bg-black" key={num} />)}
+  </div>;
+
+const Header = () => {
+  const [visible, setVisible] = useState(false);
+  
+  return (
+    <header className="flex items-center justify-between border-b border-solid border-black h-16">
+      <h1 className="text-xl sm:text-2xl m-0 px-2 sm:px-4">
+        <Link to="/">
+          <Signature />
+        </Link>
+      </h1>
+      <nav className="text-xs relative">
+        <HamburgerMenu className="sm:hidden" visible={visible} handleSetVisible={setVisible} />
+        <NavLinks visible={visible} />
+      </nav>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string
