737cfa87193afc7e551515f14639f53d1cd308f8
Yuval Allweil <themarquisdesheric@gmail.com>
2019-07-02 20:42:06 -0700

    basic lightbox complete

--- src/components/lightbox.js
+++ src/components/lightbox.js
@@ -2,30 +2,50 @@ import React, { useState } from 'react';
 import { number, object, func, arrayOf } from 'prop-types';
 import Img from 'gatsby-image';
 
-const LightBox = ({ images, imageIndex, closeLightBox }) => {
+// active link class
+
+const LightBox = ({ images, imageIndex: initialImageIndex, closeLightBox }) => {
   const [lightTheme, toggleLightTheme] = useState(true);
+  const [imageIndex, setImageIndex] = useState(initialImageIndex);
+  
+  const updateImageIndex = (operation) => {
+    let index = imageIndex || initialImageIndex;
+
+    operation === 'increment' ? index++ : index--;
+    
+    if (index === images.length) {
+      index = 0;
+    } else if (index === -1) {
+      index = images.length - 1;
+    } 
+
+    setImageIndex(index);
+  };
 
   const light = {
     backgroundColor: '#fff',
     color: '#000'
   };
-
+  
   const dark = {
     backgroundColor: '#000',
     color: '#fff'
   };
   
-  if (imageIndex === null) return null;
+  if (initialImageIndex === null) return null;
 
   return (
     <div className="relative">
       <article 
         className="lightbox"
-        onClick={closeLightBox} 
+        onClick={() => {
+          setImageIndex(null);
+          closeLightBox();
+        }} 
         style={lightTheme ? light : dark}
       >
         <span className="lightbox-close">x</span>
-        <Img fluid={images[imageIndex].node.childImageSharp.fluid} />
+        <Img fluid={images[imageIndex || initialImageIndex].node.childImageSharp.fluid} />
       </article>
       <span 
         className="lightbox-theme-toggle"
@@ -33,6 +53,18 @@ const LightBox = ({ images, imageIndex, closeLightBox }) => {
         style={{ color: lightTheme ? light.color : dark.color }}
       >
         {lightTheme ? '☾' : '🌞'}
+      </span>
+      <span 
+        className="lightbox-left-arrow"
+        onClick={() => updateImageIndex('decrement')}
+      >
+        Left
+      </span>
+      <span 
+        className="lightbox-right-arrow"
+        onClick={() => updateImageIndex('increment')}
+      >
+        Right
       </span>
     </div>
   );
