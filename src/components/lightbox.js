d8969eb395061fb5dce3a87f5cc1e069724b1f79
Yuval Allweil <themarquisdesheric@gmail.com>
2019-07-05 09:57:03 -0700

    refactor lightbox controls

--- src/components/lightbox.js
+++ src/components/lightbox.js
@@ -12,9 +12,28 @@ const dark = {
   color: '#fff'
 };
 
+let timeout;
+
 const LightBox = ({ images, imageIndex: initialImageIndex, closeLightBox }) => {
   const [imageIndex, setImageIndex] = useState(initialImageIndex);
   const [lightTheme, toggleLightTheme] = useState(true);
+  const [controlsVisible, setControlsVisible] = useState(true);
+
+  // keep controls visible if hovering over them
+  const handleMouseEnter = () => {
+    clearTimeout(timeout);
+    setControlsVisible(true);
+  };
+
+  const handleMouseMove = () => {
+    setControlsVisible(true);
+
+    if (timeout) clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      setControlsVisible(false);
+    }, 1500);
+  };
 
   const updateImageIndex = (operation) => {
     let index = imageIndex || initialImageIndex;
@@ -43,9 +62,11 @@ const LightBox = ({ images, imageIndex: initialImageIndex, closeLightBox }) => {
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
-
+    
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+
+      if (timeout) clearTimeout(timeout);
     };
   }, [handleKeyDown]);
   
@@ -55,38 +76,49 @@ const LightBox = ({ images, imageIndex: initialImageIndex, closeLightBox }) => {
     <div className="relative">
       <article 
         className="lightbox"
-        onClick={() => {
-          setImageIndex(null);
-          closeLightBox();
-        }} 
+        onMouseMove={handleMouseMove}
         style={lightTheme ? light : dark}
       >
-        <span className="lightbox-close">x</span>
         <Img fluid={images[imageIndex || initialImageIndex].node.childImageSharp.fluid} />
       </article>
-      <span 
-        className="lightbox-theme-toggle"
-        onClick={() => toggleLightTheme(!lightTheme)}
-        style={{ color: lightTheme ? light.color : dark.color }}
-      >
-        {lightTheme ? '☾' : '🌞'}
-      </span>
-      <span 
-        className="lightbox-left-arrow"
-        onClick={() => updateImageIndex('previous')}
-        role="img"
-        aria-label="previous image"
-      >
-        ⬅️
-      </span>
-      <span 
-        className="lightbox-right-arrow"
-        onClick={() => updateImageIndex('next')}
-        role="img"
-        aria-label="next image"
-      >
-        ➡️
-      </span>
+      <div className={`lightbox-controls ${controlsVisible ? 'visible' : ''}`}>
+        <button 
+          className="lightbox-close"
+          onClick={() => {
+            setImageIndex(null);
+            closeLightBox();
+          }} 
+          onMouseEnter={handleMouseEnter}
+        >
+          x
+        </button>
+        <button 
+          className="lightbox-theme-toggle"
+          onClick={() => toggleLightTheme(!lightTheme)}
+          onMouseEnter={handleMouseEnter}
+          style={{ color: lightTheme ? light.color : dark.color }}
+        >
+          <span role="img" aria-label="previous image">
+            {lightTheme ? '☾' : '🌞'}
+          </span>
+        </button>
+        <button 
+          className="lightbox-left-arrow"
+          onClick={() => updateImageIndex('previous')}
+          onMouseEnter={handleMouseEnter}
+        >
+          <span role="img" aria-label="previous image">⬅️</span>
+        </button>
+        <button 
+          className="lightbox-right-arrow"
+          onClick={() => updateImageIndex('next')}
+          onMouseEnter={handleMouseEnter}
+          role="img"
+          aria-label="next image"
+        >
+          <span role="img" aria-label="previous image">➡️</span>
+        </button>
+      </div>
     </div>
   );
 };
